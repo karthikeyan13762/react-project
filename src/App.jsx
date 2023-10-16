56b0b551830c2d0c5c2faa612c2d6f18fcabdc8c
unknown <karthikeya3762@gmail.com>
2023-10-16 12:50:24 +0530

    passing data one component to another component

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,30 @@
-function Hello(){
-return(
-  <p>Hello world</p>
-)
+function Hello(props){
+  return(
+
+    <p>Hello {props.userName } your age is {props.userAge}</p>
+  )
 }
 
-
 function App(){
-  
 
-  return (
+  let name1="Karthikeyan";
+
+  let age1=25;
+
+  let name2="Karthi";
+
+  let age2=26;
+
+  return(
 
     <div>
       <h1>Greetings</h1>
-
-      <Hello/> 
-      <Hello/>
-      <Hello/> 
-      <Hello/>
-      
+      <Hello userName ={name1} userAge ={age1} />
+      <Hello userName ={name2} userAge ={age2} />
     </div>
 
-
-
   )
+
 
 }
 export default App
