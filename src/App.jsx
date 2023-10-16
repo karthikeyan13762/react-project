57f3cd0977ea5a55999b8896e857f9319b907275
unknown <karthikeya3762@gmail.com>
2023-10-16 12:20:31 +0530

    adding dynamic content 2 a component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 function App(){
   const newDate=new Date();
-  console.log(newDate);
+  
+  const a=369
+
+  const b=700
 
   return (
 
     <div>
 
       <p>Hello world it is {newDate.toString()}</p>
+
+      <p>{a}pluse{b} is {a+b}</p>
     </div>
 
 
